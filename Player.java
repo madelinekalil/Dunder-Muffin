a902ef7e6626b31b98b451c752813680a2b89ade
madelinekalil <2006689@student.acalanes.k12.ca.us>
2017-03-15 11:18:23 -0700

    let's go

--- Player.java
+++ Player.java
@@ -7,27 +7,15 @@
  */
 public class Player
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Player
-     */
-    public Player()
+    
+    public Player(String name)
     {
-        // initialise instance variables
-        x = 0;
+        
+    }
+    
+    public boolean win() {
+        return true;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
