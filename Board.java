28410d5cb8172e5430dfec2857c9232e22433742
madelinekalil <2006689@student.acalanes.k12.ca.us>
2017-03-15 11:26:22 -0700

    day1

--- Board.java
+++ Board.java
@@ -5,17 +5,27 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.util.Scanner;
+
 public class Board 
 {
-    
+    Scanner scan = new Scanner(System.in);
+    String[][]board;
+    public int rows;
+    public int columns;
     public Board(int rows, int columns)
     {
-       String[][]board = new String[rows][columns];
+       String[][]board = new String[this.rows][this.columns];
     }
     
     public void printBoard() {
-        
-    }
-
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.println(board[i][j]);
+                System.out.println("\t");
+            }
+            System.out.println();
     
+        }
+    }
 }
